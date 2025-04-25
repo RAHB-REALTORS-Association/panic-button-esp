c48e60fd4387ca2731b5ceb4bd08545fe416cdda
Justin Hayes <justinh@rahb.ca>
2025-04-25 09:42:15 -0400

    bump version

--- PanicButton/PanicButton.ino
+++ PanicButton/PanicButton.ino
@@ -70,7 +70,7 @@ String hardwarePlatform = ""; // Store hardware platform info
 int rssi = 0;                 // WiFi signal strength in dBm
 String signalQuality = "";    // Signal quality rating
 unsigned long lastWifiCheck = 0; // Last time WiFi signal was checked
-String firmwareVersion = "1.2.0"; // Firmware version - <<<--- MAKE SURE THIS IS UPDATED WITH EACH RELEASE
+String firmwareVersion = "1.3.0"; // Firmware version - <<<--- MAKE SURE THIS IS UPDATED WITH EACH RELEASE
 
 // OTA update variables
 unsigned long lastOtaCheck = 0;
